3be400980e0879412a9d15914dbaa11062f0b0f9
padhiarmeet <padhiarmeet00@gmail.com>
2025-02-10 10:53:55 +0530

    10/02/2025

---
+++ CustomModule.py
@@ -0,0 +1,13 @@
+class Calc:
+    
+    def addTwo(n1,n2):
+        print(n1 + n2)
+    
+    def SubTwo(n1,n2):
+        print(n2 - n2)
+
+    def mulTwo(n1,n2):
+        print(n1 * n2)
+
+    def devideTwo(n1,n2):
+        print(n1 / n2)
